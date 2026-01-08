4812924b9b380d4aa70ac0b46a05143ce102bba2
millstones <40044372+scriptedcity@users.noreply.github.com>
2026-01-08 22:16:33 +0900

    設定ファイル

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+import { rehypeLinkcard } from "./src/utils/rehype-linkcard";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [rehypeLinkcard],
+  },
+  integrations: [
+    react(),
+    mdx({
+      rehypePlugins: [rehypeLinkcard],
+    }),
+    icon(),
+  ],
+  trailingSlash: "never",
   vite: {
+    resolve: {
+      alias: {
+        "@": "./src",
+      },
+    },
     plugins: [tailwindcss()],
   },
 });
